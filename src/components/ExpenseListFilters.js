88dd255df577e805c013e02d1a33b37f2eed0cf4
Bruno Wozniak <bruno.wozniak@gmail.com>
2017-10-13 09:30:09 +0200

    Update ExpenseListFilters.js

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -6,24 +6,29 @@ import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } fro
 export class ExpenseListFilters extends React.Component {
   state = {
     calendarFocused: null
-  };
+  }
+
   onDatesChange = ({ startDate, endDate }) => {
     this.props.setStartDate(startDate);
     this.props.setEndDate(endDate);
-  };
+  }
+  
   onFocusChange = (calendarFocused) => {
     this.setState(() => ({ calendarFocused }));
   }
+  
   onTextChange = (e) => {
     this.props.setTextFilter(e.target.value);
-  };
+  }
+  
   onSortChange = (e) => {
     if (e.target.value === 'date') {
       this.props.sortByDate();
     } else if (e.target.value === 'amount') {
       this.props.sortByAmount();
     }
-  };
+  }
+  
   render() {
     return (
       <div>
