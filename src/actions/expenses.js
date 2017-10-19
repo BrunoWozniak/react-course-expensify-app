1c89c6266583aec38ba6f41518258720a7633d7b
BrunoWozniak <bruno.wozniak@gmail.com>
2017-10-19 23:00:02 +0200

    Added fetch expenses from Firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -38,3 +38,26 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const fetchedExpenses = [];
+            snapshot.forEach((snapshotItem) => {
+                fetchedExpenses.push({
+                    id: snapshotItem.key,
+                    ...snapshotItem.val()
+                });
+            });
+            dispatch(setExpenses(fetchedExpenses));
+        }).catch((e) => {
+            console.log('Error!', e);
+        });
+    };
+};
