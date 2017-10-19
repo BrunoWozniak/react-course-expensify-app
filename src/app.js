57df9fa174c6c722005e39682950ede6fc9b396f
BrunoWozniak <bruno.wozniak@gmail.com>
2017-10-19 21:17:20 +0200

    Setup test database environement

--- src/app.js
+++ src/app.js
@@ -10,18 +10,9 @@ import 'react-dates/initialize';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
 const store = configureStore();
-
-// store.subscribe(() => {
-//     const state = store.getState();
-//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-//     console.log(visibleExpenses);
-// });
-
-// store.dispatch(addExpense({ description: 'Water bill', amount: 100, createdAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Gas bill', amount: 300, createdAt: 1001 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 400, createdAt: 999 }));
 
 const jsx = (
     <Provider store={store}>
