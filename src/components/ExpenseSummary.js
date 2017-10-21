6b742d69b6cbf833a9f6a8af9680a03832557b5f
BrunoWozniak <bruno.wozniak@gmail.com>
2017-10-22 00:02:54 +0200

    Added styling

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 
-export const ExpenseSummary = (props) => (
-    <div>
-        {
-        <p>
-            Viewing&nbsp;
-            {props.numberOfExpenses} expense
-            {props.numberOfExpenses > 1 ? 's' : ''} totalling&nbsp;
-            {numeral(props.totalAmount / 100).format('$0,0.00')}
-        </p>
-        }
-    </div>
-);
+export const ExpenseSummary = ({ numberOfExpenses, totalAmount }) => {
+    const expenseWord = numberOfExpenses > 1 ? 's' : '';
+    const formattedTotal = numeral(totalAmount / 100).format('$0,0.00')
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing <span>{numberOfExpenses}</span> expense{expenseWord} totalling <span>{formattedTotal}</span>
+                </h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add expense</Link>
+                </div>
+            </div>
+        </div>
+    )
+};
 
 const mapStateToProps = (state) => {
     return {
